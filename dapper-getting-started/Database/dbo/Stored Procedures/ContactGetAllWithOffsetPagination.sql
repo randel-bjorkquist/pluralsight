1f26235b279f9154180cd620e16e8a06b607fbce
Randel Bjorkquist <randel.bjorkquist@gmail.com>
2025-12-25 13:30:27 -0600

    MD check-in: shifting code versions, using dynamic sql for now; the old version is commented out, at the bottom of the file ... just in-case I change my mind again.

--- dapper-getting-started/Database/dbo/Stored Procedures/ContactGetAllWithOffsetPagination.sql
+++ dapper-getting-started/Database/dbo/Stored Procedures/ContactGetAllWithOffsetPagination.sql
@@ -1,4 +1,54 @@
 CREATE OR ALTER PROCEDURE dbo.ContactGetAllWithOffsetPagination
+   @PageNumber     AS INT           = 1
+  ,@PageSize       AS INT           = 20
+  ,@OrderByColumn  AS NVARCHAR(50)  = 'ID'
+  ,@SortDirection  AS CHAR(4)       = 'ASC'
+  ,@TotalRows      AS INT OUTPUT
+AS
+BEGIN
+  SET NOCOUNT ON;
+
+  -- Validate and normalize
+  IF @PageNumber < 1 SET @PageNumber = 1;
+  IF @PageSize   < 1 SET @PageSize   = 20;
+
+  IF @SortDirection NOT IN ('ASC', 'DESC')
+    SET @SortDirection = 'ASC';
+
+  -- Whitelist columns to prevent injection
+  IF @OrderByColumn NOT IN ('ID', 'FirstName', 'LastName', 'Email', 'Company', 'Title')
+    SET @OrderByColumn = 'ID';
+
+  DECLARE @Offset INT = (@PageNumber - 1) * @PageSize;
+
+  -- Build dynamic query
+  DECLARE @SQL NVARCHAR(MAX) = N'
+SELECT
+  ID, FirstName, LastName, Email, Company, Title 
+FROM
+  dbo.Contact 
+ORDER BY
+  [' + @OrderByColumn + N'] ' + @SortDirection;
+
+  -- Add stable tie-breaker only if not sorting by ID
+  IF @OrderByColumn <> 'ID'
+    SET @SQL += N', ID ASC';
+
+  SET @SQL += N'
+OFFSET @Offset ROWS 
+FETCH NEXT @PageSize ROWS ONLY;';
+
+  -- Execute
+  EXEC sp_executesql @SQL, N'@Offset INT, @PageSize INT', @Offset = @Offset, @PageSize = @PageSize;
+
+  -- Total count
+  SELECT @TotalRows = COUNT(*) FROM dbo.Contact;
+END
+GO
+
+/*** COMMENTED OUT: R&D CODE *********************************************************************************************
+
+CREATE OR ALTER PROCEDURE dbo.ContactGetAllWithOffsetPagination
    @PageNumber     AS INT           = 1     -- 1-based
   ,@PageSize       AS INT           = 20
   ,@OrderByColumn  AS NVARCHAR(50)  = 'ID'  -- Allowed columns
@@ -52,54 +102,4 @@ BEGIN
   SELECT @TotalRows = COUNT(*) FROM dbo.Contact;
 END
 
-/*** COMMENTED OUT: R&D CODE *********************************************************************************************
-CREATE OR ALTER PROCEDURE dbo.ContactGetAllWithOffsetPagination
-    @PageNumber     INT = 1,
-    @PageSize       INT = 20,
-    @OrderByColumn  NVARCHAR(50) = 'ID',
-    @SortDirection  CHAR(4) = 'ASC',
-    @TotalRows      INT OUTPUT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    -- Validate and normalize
-    IF @PageNumber < 1 SET @PageNumber = 1;
-    IF @PageSize < 1 SET @PageSize = 20;
-
-    IF @SortDirection NOT IN ('ASC', 'DESC')
-        SET @SortDirection = 'ASC';
-
-    -- Whitelist columns to prevent injection
-    IF @OrderByColumn NOT IN ('ID', 'FirstName', 'LastName', 'Email', 'Company', 'Title')
-        SET @OrderByColumn = 'ID';
-
-    DECLARE @Offset INT = (@PageNumber - 1) * @PageSize;
-
-    -- Build dynamic query
-    DECLARE @Sql NVARCHAR(MAX) = N'
-        SELECT 
-            ID, FirstName, LastName, Email, Company, Title
-        FROM dbo.Contact
-        ORDER BY [' + @OrderByColumn + N'] ' + @SortDirection;
-
-    -- Add stable tie-breaker only if not sorting by ID
-    IF @OrderByColumn <> 'ID'
-        SET @Sql += N', ID ASC';
-
-    SET @Sql += N'
-        OFFSET @Offset ROWS
-        FETCH NEXT @PageSize ROWS ONLY;';
-
-    -- Execute
-    EXEC sp_executesql @Sql,
-        N'@Offset INT, @PageSize INT',
-        @Offset = @Offset,
-        @PageSize = @PageSize;
-
-    -- Total count
-    SELECT @TotalRows = COUNT(*) FROM dbo.Contact;
-END
-
 ** COMMENTED OUT: R&D CODE ******************************************************************************************** */
-
